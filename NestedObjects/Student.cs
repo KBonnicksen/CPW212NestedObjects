7deff394b3f2e56e6ab90fc94e43c30f02b27fd2
Kelsey Bonnicksen <kelseybon@gmail.com>
2019-07-15 14:15:12 -0700

    Added documentation to the student class

--- NestedObjects/Student.cs
+++ NestedObjects/Student.cs
@@ -6,12 +6,25 @@ using System.Threading.Tasks;
 
 namespace NestedObjects
 {
+    /// <summary>
+    /// Represents an individual student
+    /// </summary>
     class Student
     {
+        /// <summary>
+        /// The legal first and last
+        /// ex. John Doe
+        /// </summary>
         public string FullName { get; set; }
 
+        /// <summary>
+        /// The student school provided email address
+        /// </summary>
         public string Email { get; set; }
 
+        /// <summary>
+        /// Students mailing address
+        /// </summary>
         public string HomeAddress { get; set; }
 
         /// <summary>
