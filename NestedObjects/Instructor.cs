df527873621575c0342bb3f178b34dae5399dc11
Kelsey Bonnicksen <kelseybon@gmail.com>
2019-07-17 13:28:35 -0700

    Added Instructor class

---
+++ NestedObjects/Instructor.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestedObjects
+{
+    /// <summary>
+    /// An individual instructor
+    /// </summary>
+    class Instructor
+    {
+        public Instructor()
+        {
+            CourseLoad = new List<Course>();
+        }
+
+        /// <summary>
+        /// The official @cptc.edu work email
+        /// </summary>
+        public string Email { get; set; }
+
+        /// <summary>
+        /// The location of the office in building/room format
+        /// ex. B11R112
+        /// </summary>
+        public string OfficeLocation { get; set; }
+
+        /// <summary>
+        /// The unique 4 digit extension given to each Instructor
+        /// </summary>
+        public string OfficePhone { get; set; }
+
+        /// <summary>
+        /// Legal first and last name of the instructor
+        /// </summary>
+        public string FullName { get; set; }
+
+        /// <summary>
+        /// The courses the instructor is currently teaching
+        /// </summary>
+        public List<Course> CourseLoad { get; set; } //should initialize
+
+        public void  AddCourse(Course c)
+        {
+            CourseLoad.Add(c);
+        }
+    }
+}
--- NestedObjects/Student.cs
+++ NestedObjects/Student.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//INCLUDE VALIDATION IN ALL CLASSES
 namespace NestedObjects
 {
     /// <summary>
