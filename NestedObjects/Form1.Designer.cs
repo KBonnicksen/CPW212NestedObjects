b24c16c4f63bd5fd5a4e5396b396739386909194
Kelsey Bonnicksen <kelseybon@gmail.com>
2019-07-17 13:41:31 -0700

    Designed GUI

--- NestedObjects/Form1.Designer.cs
+++ NestedObjects/Form1.Designer.cs
@@ -28,13 +28,91 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lstInstructors = new System.Windows.Forms.ListBox();
+            this.lstCourses = new System.Windows.Forms.ListBox();
+            this.lstStusents = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lstInstructors
+            // 
+            this.lstInstructors.FormattingEnabled = true;
+            this.lstInstructors.Location = new System.Drawing.Point(23, 42);
+            this.lstInstructors.Name = "lstInstructors";
+            this.lstInstructors.Size = new System.Drawing.Size(227, 212);
+            this.lstInstructors.TabIndex = 0;
+            // 
+            // lstCourses
+            // 
+            this.lstCourses.FormattingEnabled = true;
+            this.lstCourses.Location = new System.Drawing.Point(287, 42);
+            this.lstCourses.Name = "lstCourses";
+            this.lstCourses.Size = new System.Drawing.Size(227, 212);
+            this.lstCourses.TabIndex = 1;
+            // 
+            // lstStusents
+            // 
+            this.lstStusents.FormattingEnabled = true;
+            this.lstStusents.Location = new System.Drawing.Point(557, 42);
+            this.lstStusents.Name = "lstStusents";
+            this.lstStusents.Size = new System.Drawing.Size(227, 212);
+            this.lstStusents.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Instructors";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(284, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Courses";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(554, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Students";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(816, 302);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lstStusents);
+            this.Controls.Add(this.lstCourses);
+            this.Controls.Add(this.lstInstructors);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox lstInstructors;
+        private System.Windows.Forms.ListBox lstCourses;
+        private System.Windows.Forms.ListBox lstStusents;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
